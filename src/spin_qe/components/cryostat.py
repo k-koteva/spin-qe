4482c3a88798b1bfe154f93cc7861f0fe07a29bc
Konstantina Koteva <koteva.k.i@gmail.com>
2023-10-16 19:25:44 +0200

    cryostat almost complete

--- src/spin_qe/components/cryostat.py
+++ src/spin_qe/components/cryostat.py
@@ -1,11 +1,15 @@
-from typing import List, Optional
+import os
+from pathlib import Path
+from typing import List, Optional, Union
 
+import matplotlib.pyplot as plt
 import numpy as np
 from loguru import logger
 from pandas import DataFrame
 from pydantic import BaseModel, Field, validator
 
 from spin_qe.validation.attenuation import Atten
+from spin_qe.validation.power import Power
 from spin_qe.validation.temperature import Temp
 
 
@@ -15,6 +19,7 @@ class Cryo(BaseModel):
     attens: List[float]
     Si_abs: Optional[float] = Field(0, ge=0, le=1)
     stages: DataFrame = DataFrame()
+    efficiency: str = 'Carnot'
 
     @validator("Tq", pre=True, always=True)
     def validate_Tq(cls, value):  # pylint: disable=no-self-argument
@@ -44,10 +49,16 @@ class Cryo(BaseModel):
         # Remove entries where temperature is lower or equal to Tq
         zipped_dict = {k: v for k, v in zipped_dict.items() if k > Tq}
 
-        si_abs = round(Atten.val(frac=(1 - Si_abs), convert_to='dB'), 2)
+        si_abs = round(Atten.val(frac=(1 - Si_abs), convert_to='dB'), 5)
 
         # Add Tq: Si_abs as a key-value pair
         zipped_dict[Tq] = si_abs
+        logger.critical(f"Chip attenuation: {si_abs}")
+
+        if zipped_dict.get(300, 0) != 0:
+
+            zipped_dict[300] = 0
+            logger.info("Room temperature attenuation set to 0.")
 
         # Sort keys and create the desired list
         sorted_keys = sorted(zipped_dict.keys(), reverse=True)
@@ -64,20 +75,47 @@ class Cryo(BaseModel):
 
         return df
 
+    def eff(self, stage_T: float):
+        if self.efficiency != 'Carnot':
+            raise ValueError("Unknown efficiency")
+        return (300 - stage_T) / stage_T
+
     def heat_evacuated_at_stage(self, temp: float, power: float) -> float:
-        indices_for_temp = self.stages.index[self.stages['temps'] == temp].tolist(
-        )
+        if temp == 300:
+            # Special case when temperature is 300
+            all_attenuations = self.stages['attens'].tolist()
+            summed_attenuation = sum(all_attenuations)
+            fraction = Atten.val(dB=summed_attenuation, convert_to='frac')
+        else:
+            indices_for_temp = self.stages.index[self.stages['temps'] == temp].tolist(
+            )
+            if not indices_for_temp:
+                raise ValueError(f"No stages found for temperature {temp}.")
 
-        if not indices_for_temp:
-            raise ValueError(f"No stages found for temperature {temp}.")
+            sums = []
+            for index in indices_for_temp:
 
-        sums = []
-        for index in indices_for_temp:
-            summed = self.stages.loc[:index, 'attens'].sum()
-            sums.append(summed)
+                # Calculate sum including the current index and convert to fraction
+                summed_with_current = self.stages.loc[:index, 'attens'].sum()
+                fraction_with_current = Atten.val(
+                    dB=summed_with_current, convert_to='frac')
+                assert isinstance(fraction_with_current, float)
 
-        fractions = [Atten.val(dB=atten, convert_to='frac') for atten in sums]
-        answer = power * np.sum(fractions)
+                # Calculate sum excluding the current index and convert to fraction
+                summed_excluding_current = self.stages.loc[:index -
+                                                           1, 'attens'].sum()
+                fraction_excluding_current = Atten.val(
+                    dB=summed_excluding_current, convert_to='frac')
+                assert isinstance(fraction_excluding_current, float)
+
+                # Calculate the difference between the two fractions (with positions exchanged)
+                diff = fraction_excluding_current - fraction_with_current
+
+                sums.append(diff)
+
+            fraction = np.sum(sums)
+        assert isinstance(fraction, float)
+        answer = power * fraction
 
         return answer
 
@@ -85,50 +123,277 @@ class Cryo(BaseModel):
         total_heat = 0
         for temp in self.stages['temps'].unique():
             new_heat = self.heat_evacuated_at_stage(temp=temp, power=power)
-            logger.info(f"New heat: {new_heat}")
             total_heat += new_heat
+        logger.info(f"Total heat should be equal to power: {total_heat}")
         return total_heat
+
+    def power_to_evacuate_heat_at_stage(self, temp: float, power: float) -> float:
+        efficiency = self.eff(temp)
+        heat_at_stage = self.heat_evacuated_at_stage(temp, power)
+        return efficiency * heat_at_stage
+
+    def total_power(self, power: float) -> float:
+        total_power = 0
+        for temp in self.stages['temps'].unique():
+            total_power += self.power_to_evacuate_heat_at_stage(temp, power)
+        return total_power
+
+    def plot_heat_evacuated_vs_temperature(self, power: float):
+        unique_temps = self.stages['temps'].unique()
+        heat_evacuated = []
+
+        for temp in unique_temps:
+            heat_stage = self.heat_evacuated_at_stage(temp, power)
+            heat_evacuated.append(heat_stage)
+
+        plt.bar(unique_temps, heat_evacuated, align='center', alpha=0.7)
+        plt.xlabel('Temperature (K)')
+        plt.ylabel('Heat Evacuated')
+        plt.title('Heat Evacuated vs Temperature')
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+        plt.show()
 
     class Config:
         arbitrary_types_allowed = True
 
 
+def overlay_heat_evacuated_plots(cryo_instances, power):
+    plt.figure(figsize=(10, 6))
+
+    for cryo_instance in cryo_instances:
+        unique_temps = cryo_instance.stages['temps'].unique()
+        heat_evacuated = []
+
+        for temp in unique_temps:
+            heat_stage = cryo_instance.heat_evacuated_at_stage(temp, power)
+            heat_evacuated.append(heat_stage)
+
+        plt.bar(unique_temps, heat_evacuated, align='center',
+                alpha=0.7, label=f'Tq={cryo_instance.Tq}')
+
+    plt.xlabel('Temperature (K)')
+    plt.ylabel('Heat Evacuated')
+    plt.title('Heat Evacuated vs Temperature for Different Cryo Instances')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.legend()
+    plt.show()
+
+
+def plot_total_power_vs_Si_abs_and_Tq(
+    Tq_values: List[float],
+    temps: List[float],
+    attens: List[float],
+    Si_abs_range: List[float],
+    input_power: float = 10.0,
+    save_dir: Optional[Union[Path, str]] = None
+) -> None:
+    plt.figure(figsize=(10, 6))
+
+    for Tq in Tq_values:
+        total_powers = []
+
+        for Si_abs in Si_abs_range:
+            # Create a Cryo instance with Si_abs=0.0 for reference power calculation
+            reference_cryo = Cryo(
+                Tq=Tq,
+                temps=temps,
+                attens=attens,
+                Si_abs=0.0
+            )
+
+            # Calculate reference power with Si_abs=0.0
+            reference_power = reference_cryo.total_power(power=input_power)
+
+            # Create a Cryo instance with the provided Si_abs for the main calculation
+            main_cryo = Cryo(
+                Tq=Tq,
+                temps=temps,
+                attens=attens,
+                Si_abs=Si_abs
+            )
+
+            # Calculate power with the provided Si_abs
+            calculated_power = main_cryo.total_power(power=input_power)
+
+            # Use the calculated power only if it's higher than the minimum power
+            if calculated_power >= reference_power:
+                total_powers.append(calculated_power)
+                logger.info('Calculated power used')
+            else:
+                total_powers.append(reference_power)
+                logger.info('Minimum power used')
+
+        W_list = Power.val(mW=total_powers,  # pylint: disable=invalid-name
+                           convert_to='W')
+        plt.plot(Si_abs_range, W_list, label=f'Tq={np.round(Tq,3)}K')
+
+    pwd = os.getcwd()
+
+    # Define the 'results' folder path
+    results_dir = os.path.join(pwd, 'results')
+
+    plt.xlabel('Si abs')
+    plt.ylabel('Total Power (W)')
+    plt.title(
+        f'Total Power vs Si abs for Different Tq Values and {int(input_power)}mW input power')
+    plt.grid(True)
+    plt.ylim(0, None)
+    plt.legend()
+
+    if save_dir is not None:
+        # Specify the file path to save the PDF file
+        file_name = f'Total_Power_vs_Si_abs_and{int(input_power)}mW input power saturation.pdf'
+        file_path = Path(results_dir) / file_name
+
+        # Save the figure as a PDF image
+        plt.savefig(file_path, format='pdf')
+
+    # Optionally, you can check if the PDF image was saved successfully
+        if file_path.exists():
+            print(f"PDF image saved to {file_path}")
+        else:
+            print(f"Failed to save PDF image to {file_path}")
+    else:
+        plt.show()
+
+    plt.close()
+
+
 def main():
-    try:
-        cryo_instance = Cryo(
-            Tq=100,
-            temps=[100, 200, 150, 300],
-            attens=[10, 20, 15, 30],
-            Si_abs=0.5
-        )
+    # Tq = 0.04
+    # temps = [4]
+    # attens = [2]
+    # Si_abs_range = np.linspace(0.01, 0.99, 100)
 
-        logger.info(f"Tq: {cryo_instance.Tq}")
-        logger.info(f"Temps: {cryo_instance.temps}")
-        logger.info(f"Attens: {cryo_instance.attens}")
-        logger.info(f"Si_abs: {cryo_instance.Si_abs}")
-        logger.info(f"Stages: {cryo_instance.stages}")
+    # plot_total_power_vs_Si_abs(Tq, temps, attens, Si_abs_range)
 
-        heat_at_stage = cryo_instance.heat_evacuated_at_stage(
-            temp=200, power=5)
-        logger.info(f"Heat evacuated at stage with temp 200: {heat_at_stage}")
+    Tq_values = [0.006, 0.02, 0.04, 0.1, 1]
+    Tq = 0.02
+    temps = [4]
+    attens = [3.01]
+    # temps = [4, 0.8, 0.1]
+    # attens = [10, 3, 0.0001]
+    Si_abs_range = np.linspace(0.0001, 0.01, 100)
 
-        total_heat = cryo_instance.total_heat_evacuated(power=1)
-        logger.info(f"Total heat evacuated: {total_heat}")
+    plot_total_power_vs_Si_abs_and_Tq(Tq_values, temps, attens, Si_abs_range)
+    # plot_heat_per_stage_vs_Si_abs(Tq, temps, attens, Si_abs_range)
 
-        another_cryo_instance = Cryo(
-            Tq=5,
-            temps=[9, 300],
-            attens=[3.01, 0],
-            Si_abs=0
-        )
-        another_cryo_instance.total_heat_evacuated(power=1)
-        logger.info(
-            f"Total heat evacuated small: {another_cryo_instance.total_heat_evacuated(power=1)}")
-        si_abs = round(Atten.val(frac=(1 - 0.1), convert_to='dB'), 2)
-        logger.info(si_abs)
+    # Example usage:
+    # cryo_instance1 = Cryo(
+    #     Tq=0.06,
+    #     temps=[4],
+    #     attens=[3.01],
+    #     Si_abs=0.0
+    # )
 
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
+    # cryo_instance2 = Cryo(
+    #     Tq=0.06,
+    #     temps=[4],
+    #     attens=[3.01],
+    #     Si_abs=0.5
+    # )
+
+    # power = 1  # Adjust the power as needed
+
+    # overlay_heat_evacuated_plots([cryo_instance1, cryo_instance2], power)
+    # cryo_instance = Cryo(
+    #     Tq=0.06,
+    #     temps=[4],
+    #     attens=[3.01],
+    #     Si_abs=0.5
+    # )
+
+    # power = 1  # Adjust the power as needed
+
+    # cryo_instance.plot_heat_evacuated_vs_temperature(power)
+
+    # Example usage:
+#     Tq = 0.04  # Specify Tq here
+#    # Tq = 0.02
+#     temps = [4]
+#     attens = [3.01]
+#     power = 5  # Adjust the power as needed
+
+#     plot_heat_evacuated_vs_temperature(Tq, temps, attens, power)
+
+    # try:
+    #     cryo_instance = Cryo(
+    #         Tq=100,
+    #         temps=[100, 200, 150, 300],
+    #         attens=[10, 20, 15, 0],
+    #         Si_abs=0.5
+    #     )
+
+    #     logger.info(f"Tq: {cryo_instance.Tq}")
+    #     logger.info(f"Temps: {cryo_instance.temps}")
+    #     logger.info(f"Attens: {cryo_instance.attens}")
+    #     logger.info(f"Si_abs: {cryo_instance.Si_abs}")
+    #     logger.info(f"Stages: {cryo_instance.stages}")
+
+    #     heat_at_stage = cryo_instance.heat_evacuated_at_stage(
+    #         temp=200, power=5)
+    #     logger.info(f"Heat evacuated at stage with temp 200: {heat_at_stage}")
+
+    #     total_heat = cryo_instance.total_heat_evacuated(power=1)
+    #     logger.info(f"Total heat evacuated: {total_heat}")
+
+    #     another_cryo_instance = Cryo(
+    #         Tq=5,
+    #         temps=[9],
+    #         attens=[3.01],
+    #         Si_abs=0.00
+    #     )
+    #     another_cryo_instance.total_heat_evacuated(power=1)
+    #     logger.info(
+    #         f"Total heat evacuated should equal the: {another_cryo_instance.total_heat_evacuated(power=1)}")
+
+    #     and_another_cryo_instance = Cryo(
+    #         Tq=5,
+    #         temps=[9, 20, 30],
+    #         attens=[15, 20, 30],
+    #         Si_abs=0.5
+    #     )
+    #     and_another_cryo_instance.total_heat_evacuated(power=1)
+    #     logger.info(
+    #         f"Total heat evacuated should equal the: {and_another_cryo_instance.total_heat_evacuated(power=1)}")
+
+    # except Exception as e:
+    #     logger.error(f"An error occurred: {e}")
+
+    # try:
+    #     cryo_instance = Cryo(
+    #         Tq=0.06,
+    #         temps=[4, 10],
+    #         attens=[3, 3],
+    #         Si_abs=0.5
+    #     )
+
+    #     logger.info(f"Tq: {cryo_instance.Tq}")
+    #     logger.info(f"Temps: {cryo_instance.temps}")
+    #     logger.info(f"Attens: {cryo_instance.attens}")
+    #     logger.info(f"Si_abs: {cryo_instance.Si_abs}")
+    #     logger.info(f"Stages: {cryo_instance.stages}")
+
+    #     heat_at_stage = cryo_instance.heat_evacuated_at_stage(
+    #         temp=4, power=5)
+    #     logger.info(f"Heat evacuated at stage with temp 200: {heat_at_stage}")
+
+    #     total_heat = cryo_instance.total_heat_evacuated(power=1)
+    #     logger.info(f"Total heat evacuated: {total_heat}")
+
+    #     # Test power_to_evacuate_heat_at_stage and total_power
+    #     temp_to_test = 4
+    #     power_to_test = 1
+    #     power_at_stage = cryo_instance.power_to_evacuate_heat_at_stage(
+    #         temp=temp_to_test, power=power_to_test)
+    #     logger.info(
+    #         f"Power required at stage with temp {temp_to_test}: {power_at_stage}")
+
+    #     total_power = cryo_instance.total_power(power=power_to_test)
+    #     logger.info(f"Total power required: {total_power}")
+
+    # except Exception as e:
+    #     logger.error(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
@@ -158,3 +423,6 @@ if __name__ == "__main__":
 #
 # Very important: be able to change the value of reflectivity and make sure everything else is updated accrodingly
 # add test functions to this effect (in all files for that matter)
+
+
+# Example usage:
