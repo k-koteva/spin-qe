6dc8901ae96b2a8e8a88ef51847a863f718e22cc
Konstantina Koteva <koteva.k.i@gmail.com>
2024-05-23 14:37:41 +0200

    optimisation file sorted but still not working for spin qubits global

--- src/spin_qe/optimisation/optimisation.py
+++ src/spin_qe/optimisation/optimisation.py
@@ -1,12 +1,14 @@
+from typing import List, Tuple
+
 import matplotlib.pyplot as plt
 import numpy as np
+from loguru import logger
 from matplotlib.colors import LogNorm
 
-# Assuming spin_qe is the module where SpinQubit is defined
 import spin_qe.device.spin_qubits as sq
 
 
-def powerNoise_calcSQRab(tqb, rabifreq):
+def calculate_power_noise(tqb: np.ndarray, rabifreq: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     attens = [3, 0]
     stage_ts = [4, 300]
     silicon_abs = 0.01
@@ -15,74 +17,79 @@ def powerNoise_calcSQRab(tqb, rabifreq):
     fidelityGrid = np.zeros((len(rabifreq), len(tqb)))
 
     for i, rabi in enumerate(rabifreq):
-        print('Rabi frequency = ', rabi)
+        logger.info(f'Rabi frequency = {rabi}')
         for j, tq in enumerate(tqb):
-            print('Temperature = ', tq)
-            mySQ = sq.SpinQubit(Tq=tq, rabi = rabi, rabi_in_MHz=rabi*1e6, atts_list=attens,
+            logger.info(f'Temperature = {tq}')
+            mySQ = sq.SpinQubit(Tq=tq, rabi=rabi, rabi_in_MHz=rabi * 1e6, atts_list=attens,
                                 stages_ts=stage_ts, silicon_abs=silicon_abs)
-            # Using total_power with PowPi as input
             powerGrid[i, j] = mySQ.total_power()
-            # Using fid_1q method for fidelity
             fidelityGrid[i, j] = mySQ.fid_1q()
-            print('fid = ', mySQ.fid_1q())
+            logger.info(f'fid = {mySQ.fid_1q()}')
 
     return powerGrid, fidelityGrid
 
-tqb = np.linspace(0.01,1,10)
-rabifreq = np.linspace(0.01,15,10)
-R,T = np.meshgrid(rabifreq,tqb,indexing='ij')
-powerful, smoothFid = powerNoise_calcSQRab(tqb,rabifreq)
+def optimize_power_and_efficiency(powerful: np.ndarray, smoothFid: np.ndarray, metric_target: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    opt_power = 1e28 * np.ones(len(metric_target))
+    gate_efficiency = np.zeros(len(metric_target))
 
-metric_target = np.linspace(0.90,0.999,30)
-opt_power = 1e28*np.ones(len(metric_target))
-gate_efficiency = np.zeros(len(metric_target))
+    for k in range(len(metric_target)):
+        for i in range(len(powerful)):
+            for j in range(len(powerful[i])):
+                if smoothFid[i, j] >= metric_target[k]:
+                    if opt_power[k] > powerful[i, j]:
+                        opt_power[k] = powerful[i, j]
+        if opt_power[k] == 1e28:
+            opt_power[k] = np.nan
+        else:
+            gate_efficiency[k] = metric_target[k] / opt_power[k]
 
-for k in range(len(metric_target)):
-    for i in range(len(rabifreq)):
-        for j in range(len(tqb)):
-            if smoothFid[i,j] >= metric_target[k]:
-                if opt_power[k] > powerful[i,j]:
-                    opt_power[k] = powerful[i,j]
-    if opt_power[k] == 1e28:
-        opt_power[k] = np.nan
-    else:
-        gate_efficiency[k] = metric_target[k]/opt_power[k]
+    return opt_power, gate_efficiency
 
-mylevels = [0.90, 0.99, 0.997]
-mkl = {0.90: '$0.90$', 0.99: '$0.99$',0.997: '$0.997$'}
-fig = plt.figure()
-ax0 = fig.gca()
-powerPlot = ax0.pcolor(R,T,powerful, 
-           norm=LogNorm(vmin=float(np.nanmin(powerful)), vmax=float(np.nanmax(powerful))),
-           shading="auto")
+def plot_results(R: np.ndarray, T: np.ndarray, powerful: np.ndarray, smoothFid: np.ndarray, fid_levels: List[float], power_levels: List[float], filename: str):
+    mkl_fid = {level: f'${level}$' for level in fid_levels}
+    mkl_power = {level: f'${level}$' for level in power_levels}
 
-# noiseContour = ax0.contour(R,T,noisy
-#                 ,mylevels
-#                 ,norm=LogNorm(vmin=np.nanmin(powerful),
-#                               vmax=np.nanmax(powerful) )
-#                 ,colors="red"
-#                 )
-smoothFid = np.where(smoothFid <= 0, 1e-10, smoothFid)
-noiseContour = ax0.contour(R,T,smoothFid
-                ,mylevels
-                ,norm=LogNorm(vmin=float(np.nanmin(smoothFid)) + 1e-10,
-                              vmax=float(np.nanmax(smoothFid)) + 1e-10 )
-                ,colors="black"
-                )
+    fig = plt.figure()
+    ax0 = fig.gca()
+    powerPlot = ax0.pcolor(R, T, powerful, 
+                           norm=LogNorm(vmin=float(np.nanmin(powerful)), vmax=float(np.nanmax(powerful))),
+                           shading="auto")
 
-clabel = ax0.clabel(noiseContour,levels=mylevels
-        ,fmt=mkl 
-        ,fontsize=10, inline=True)
-# clabel = ax0.clabel(smoothFid,levels=mylevels
-#         ,fmt=mkl
-#         ,fontsize=10, inline=True)
+    smoothFid = np.where(smoothFid <= 0, 1e-10, smoothFid)
+    
+    fid_contour = ax0.contour(R, T, smoothFid, fid_levels,
+                              norm=LogNorm(vmin=float(np.nanmin(smoothFid)) + 1e-10,
+                                           vmax=float(np.nanmax(smoothFid)) + 1e-10),
+                              colors="black")
 
-ax0.set_yscale('log')
-# ax0.set_xscale('log')
-cbar = fig.colorbar(powerPlot,ax=ax0)
+    power_contour = ax0.contour(R, T, powerful, power_levels,
+                                norm=LogNorm(vmin=float(np.nanmin(powerful)),
+                                             vmax=float(np.nanmax(powerful))),
+                                colors="white")
 
-ax0.set_ylabel('Temperature of quantum chip /K',fontsize=14)
-ax0.set_xlabel('Rabi frequency /MHz',fontsize=14)
-cbar.ax.set_ylabel('Power /W',fontsize=12)
-fig.savefig('spinQubit_optimPowSiAbs001realTRIAL_check.pdf',bbox_inches='tight')
+    ax0.clabel(fid_contour, levels=fid_levels, fmt=mkl_fid, fontsize=10, inline=True)
+    ax0.clabel(power_contour, levels=power_levels, fmt=mkl_power, fontsize=10, inline=True)
 
+    ax0.set_yscale('log')
+    cbar = fig.colorbar(powerPlot, ax=ax0)
+    ax0.set_ylabel('Temperature of quantum chip /K', fontsize=14)
+    ax0.set_xlabel('Rabi frequency /MHz', fontsize=14)
+    cbar.ax.set_ylabel('Power /W', fontsize=12)
+    fig.savefig(filename, bbox_inches='tight')
+
+def main():
+    tqb = np.linspace(0.01, 1, 10)
+    rabifreq = np.linspace(0.01, 15, 10)
+    R, T = np.meshgrid(rabifreq, tqb, indexing='ij')
+    
+    powerful, smoothFid = calculate_power_noise(tqb, rabifreq)
+    
+    metric_target = np.linspace(0.90, 0.999, 30)
+    opt_power, gate_efficiency = optimize_power_and_efficiency(powerful, smoothFid, metric_target)
+    
+    fid_levels = [0.90, 0.99, 0.997]
+    power_levels = [1e-3, 10]
+    plot_results(R, T, powerful, smoothFid, fid_levels, power_levels, 'spinQubit_optimPowSiAbs001realTRIAL_check.pdf')
+
+if __name__ == "__main__":
+    main()
