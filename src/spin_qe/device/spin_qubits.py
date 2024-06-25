3aac462080c956add9ef6ed121a9b29e32c9a84c
k-koteva <koteva.k.i@gmail.com>
2024-06-25 11:57:10 +0800

    Plots update

--- src/spin_qe/device/spin_qubits.py
+++ src/spin_qe/device/spin_qubits.py
@@ -11,7 +11,7 @@ from spin_qe.components.cryostat import Cryo
 
 
 class SpinQubit(BaseModel):
-    n_q: int = conint(ge=1, le=20)
+    n_q: int = conint(ge=1, le=10000000000000000)
     Tq: float = confloat(gt=0.0, le=300)
     f: float = Field(39.33e9, alias='f_in_GHz')
     rabi: float = Field(0.5e6, alias='rabi_in_MHz')
--- src/spin_qe/optimisation/optimisation.py
+++ src/spin_qe/optimisation/optimisation.py
@@ -60,7 +60,8 @@ def optimize_power_and_efficiency(powerful: np.ndarray, smoothFid: np.ndarray, m
 
 def plot_results(R: np.ndarray, T: np.ndarray, powerful: np.ndarray, smoothFid: np.ndarray, fid_levels: List[float], power_levels: List[float], filename: str, plot_energy: bool = False):
         # Define the path to the Results folder
-    dropbox_folder = os.path.expanduser('~/Dropbox/Apps/Overleaf/Energetics NISQ Spin qubits/Results')
+    # dropbox_folder = os.path.expanduser('~/Dropbox/Apps/Overleaf/Energetics NISQ Spin qubits/Results')
+    dropbox_folder = os.path.expanduser('~/Dropbox/Apps/Overleaf/QEIW Abstract')
     
     # Create the Results folder if it doesn't exist
     os.makedirs(dropbox_folder, exist_ok=True)
@@ -70,21 +71,10 @@ def plot_results(R: np.ndarray, T: np.ndarray, powerful: np.ndarray, smoothFid: 
     
     # Create the local results folder if it doesn't exist
     os.makedirs(results_folder, exist_ok=True)
-    
-    # # Add svg to the base filename for Dropbox
-    # svg_filename = filename + '.svg'
-    
-    # # Add pdf to the base filename for local folder
-    # pdf_filename = filename + '.pdf'
-    
-    # Full paths for saving the files
-    # dropbox_full_path = os.path.join(dropbox_folder, svg_filename)
-    # local_full_path = os.path.join(results_folder, pdf_filename)
-
 
     mkl_fid = {level: f'${level}$' for level in fid_levels}
     mkl_power = {level: f'${level}$' for level in power_levels}
-    label = 'Energy /J' if plot_energy else 'Power /W'
+    label = 'Energy per gate (J)' if plot_energy else 'Power /W'
     fig = plt.figure()
     ax0 = fig.gca()
     powerPlot = ax0.pcolor(R, T, powerful, 
@@ -96,23 +86,32 @@ def plot_results(R: np.ndarray, T: np.ndarray, powerful: np.ndarray, smoothFid: 
     fid_contour = ax0.contour(R, T, smoothFid, fid_levels,
                               norm=LogNorm(vmin=float(np.nanmin(smoothFid)) + 1e-10,
                                            vmax=float(np.nanmax(smoothFid)) + 1e-10),
-                              colors="black")
+                              colors="black", linewidths=1)
 
     power_contour = ax0.contour(R, T, powerful, power_levels,
                                 norm=LogNorm(vmin=float(np.nanmin(powerful)),
                                              vmax=float(np.nanmax(powerful))),
-                                colors="white")
+                                colors="white", linewidths=1)
 
-    ax0.clabel(fid_contour, levels=fid_levels, fmt=mkl_fid, fontsize=10, inline=True)
-    ax0.clabel(power_contour, levels=power_levels, fmt=mkl_power, fontsize=10, inline=True)
-
+    ax0.clabel(fid_contour, levels=fid_levels, fmt=mkl_fid, fontsize=9, inline=True)
+    ax0.clabel(power_contour, levels=power_levels, fmt=mkl_power, fontsize=9, inline=True)
+    # ax0.set_title('Driving 1 qubit')
     ax0.set_yscale('log')
+    ax0.set_xscale('log')
     cbar = fig.colorbar(powerPlot, ax=ax0)
-    ax0.set_ylabel('Temperature of quantum chip /K', fontsize=14)
-    ax0.set_xlabel('Rabi frequency /MHz', fontsize=14)
-    
-    cbar.ax.set_ylabel(label, fontsize=12)
+    ax0.set_ylabel('Temperature of quantum chip (K)', fontsize=15)
+    ax0.set_xlabel('Rabi frequency (MHz)', fontsize=15)
 
+    cbar.ax.set_ylabel(label, fontsize=15)
+    cbar.ax.tick_params(axis='both', which='major', length=8, width=1.5, labelsize=12)
+    cbar.ax.tick_params(axis='both', which='minor', length=4, width=1, labelsize=10)
+
+    h1,_ = fid_contour.legend_elements()
+    h2,_ = power_contour.legend_elements()
+
+    ax0.legend([h1[0], h2[0]], ['Gate\nfidelity', 'Energy\nper\ngate'], loc='upper left', fontsize=8, ncol=1, bbox_to_anchor=(0.05, 1))
+    ax0.tick_params(axis='both', which='major', top=True, right=True,  direction='in', length=8, width=1.5, labelsize=12)
+    ax0.tick_params(axis='both', which='minor', top=True, right=True, direction='in', length=4, width=1, labelsize=10)
         # Save the plot as SVG in Dropbox folder
     fig.savefig(os.path.join(dropbox_folder, filename + '.svg'), bbox_inches='tight')
     fig.savefig(os.path.join(dropbox_folder, filename + '.pdf'), bbox_inches='tight')
@@ -139,14 +138,40 @@ def plot_results(R: np.ndarray, T: np.ndarray, powerful: np.ndarray, smoothFid: 
 #     plt.savefig(filename, bbox_inches='tight')
 
 def plot_energy_vs_temperature(tqb: np.ndarray, energyCarnot: np.ndarray, energySmall: np.ndarray, filename: str):
-    plt.figure()
-    plt.plot(tqb, energyCarnot, label='Carnot Efficiency')
-    plt.plot(tqb, energySmall, label='Small System Efficiency', linestyle='--')
-    plt.xlabel('Temperature of quantum chip /K')
+    dropbox_folder = os.path.expanduser('~/Dropbox/Apps/Overleaf/Energetics NISQ Spin qubits/Results')
+    # Create the Results folder if it doesn't exist
+    os.makedirs(dropbox_folder, exist_ok=True)
+    plt.title('1 Qubit at 10MHz', fontsize=16) 
+    plt.plot(tqb, energyCarnot, label='Carnot Efficiency', linewidth=2.5)
+    plt.plot(tqb, energySmall, label='Small System Efficiency', linestyle='--', linewidth=2.5)
+    plt.xlabel('Temperature of quantum chip /K', fontsize=15)
+    plt.xticks(fontsize=17)  # Increased tick size
+    plt.yticks(fontsize=17)
     plt.xscale('log')
-    plt.ylabel('Energy /J')
-    plt.legend()
-    plt.savefig(filename, bbox_inches='tight')
+    plt.yscale('log')
+    plt.ylabel('Energy from Cryogenics /J', fontsize=15)
+    plt.legend(fontsize=15)
+    plt.tick_params(axis='both', which='both', direction='in', top=True, right=True, width=2.5)  # Ticks on top and right
+    plt.savefig(os.path.join(filename + '.pdf'), bbox_inches='tight')
+    plt.savefig(os.path.join(dropbox_folder, filename + '.pdf'), bbox_inches='tight')
+
+def plot_energy_vs_rabi(rabi: np.ndarray, energyCarnot: np.ndarray, energySmall: np.ndarray, filename: str):
+    dropbox_folder = os.path.expanduser('~/Dropbox/Apps/Overleaf/Energetics NISQ Spin qubits/Results')
+    # Create the Results folder if it doesn't exist
+    os.makedirs(dropbox_folder, exist_ok=True)
+    plt.title('1 Qubit at 10MHz', fontsize=16) 
+    plt.plot(rabi, energyCarnot, label='Carnot Efficiency', linewidth=2.5)
+    plt.plot(rabi, energySmall, label='Small System Efficiency', linestyle='--', linewidth=2.5)
+    plt.xlabel('Rabi frequency /K', fontsize=15)
+    plt.xticks(fontsize=17)  # Increased tick size
+    plt.yticks(fontsize=17)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.ylabel('Energy from Cryogenics (J)', fontsize=15)
+    plt.legend(fontsize=15)
+    plt.tick_params(axis='both', which='both', direction='in', top=True, right=True, width=2.5)  # Ticks on top and right
+    plt.savefig(os.path.join(filename + '.pdf'), bbox_inches='tight')
+    plt.savefig(os.path.join(dropbox_folder, filename + '.pdf'), bbox_inches='tight')
 
 def find_min_and_index(values: list) -> Tuple[Any, int]:
     min_value = min(values)
@@ -154,8 +179,11 @@ def find_min_and_index(values: list) -> Tuple[Any, int]:
     return min_value, min_index
 
 def main():
+    nq = 100
     tqb = np.logspace(np.log10(0.06), np.log10(10), 100)
-    rabifreq = np.linspace(0.01, 15, 100)
+    # rabifreq = np.linspace(0.01, 100, 100)
+    rabifreq = np.logspace(np.log10(0.01), np.log10(100), 100)
+
     R, T = np.meshgrid(rabifreq, tqb, indexing='ij')
     eff = 'Carnot'
     powerful, smoothFid, conductionP, cryoP, energyTotal, conductionE, cryoE = calculate_power_noise(eff, tqb, rabifreq)
@@ -163,7 +191,7 @@ def main():
     metric_target = np.linspace(0.90, 0.999, 30)
     opt_power, gate_efficiency = optimize_power_and_efficiency(powerful, smoothFid, metric_target)
     
-    fid_levels: List[float] = [0.90, 0.99, 0.997]
+    fid_levels: List[float] = [0.9, 0.996, 0.9999]
     power_levels: List[float] = [1, 10, 30]
     energy_levels: List[float] = [1e-8, 1e-7, 1e-6]
     # energy_levels: List[float] = [1e-6, 1e-5, 1e-4]
@@ -178,18 +206,26 @@ def main():
 
     #  Extract power values at Rabi frequency of 10 MHz
 
-    # rabi_index = np.argmin(np.abs(rabifreq - 10))
-    # # powerCarnot = powerful[rabi_index, :]
+    rabi_index = np.argmin(np.abs(rabifreq - 10))
+    # powerCarnot = powerful[rabi_index, :]
     # energyCarnot = energyTotal[rabi_index, :]
-    # eff='Small System'
-    # powerful, smoothFid, conductionP, cryoP, energyTotal, conductionE, cryoE = calculate_power_noise(eff, tqb, rabifreq)
-    # # powerSmall = powerful[rabi_index, :]
-    # energySmall = energyTotal[rabi_index, :]
-    # # plot_power_vs_temperature(tqb, powerCarnot, powerSmall, 'power_vs_temperature.pdf')
-
-    #     # Extract energy values at Rabi frequency of 10 MHz
+    energyCarnot = cryoE[rabi_index, :]
     
-    # plot_energy_vs_temperature(tqb, energyCarnot, energySmall, 'energy_vs_temperature_compare.pdf')
+    eff='Small System'
+    powerful, smoothFid, conductionP, cryoP, energyTotal, conductionE, cryoE = calculate_power_noise(eff, tqb, rabifreq)
+    # powerSmall = powerful[rabi_index, :]
+    # energySmall = energyTotal[rabi_index, :]
+    energySmall = cryoE[rabi_index, :]
+    # plot_power_vs_temperature(tqb, powerCarnot, powerSmall, 'power_vs_temperature.pdf')
+
+        # Extract energy values at Rabi frequency of 10 MHz
+    
+    # plot_energy_vs_temperature(tqb, energyCarnot, energySmall, '1Q_cryogenics_energy_vs_temperature_compare')
+
+
+    # #Extract energy values at Temp = 700mk
+    # temp_index = np.argmin(np.abs(tqb - 10))
+    # energyCarnot = cryoE[rabi_index, :]
 
 
 if __name__ == "__main__":
